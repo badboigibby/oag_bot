68e65c801612acba917fc89dece35c1d594027ef
badboigibby <83539684+badboigibby@users.noreply.github.com>
2025-01-22 00:00:17 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
+# Load environment variables from a .env file
 load_dotenv()
 
-# Get the bot token
+# Get the bot token from environment variables
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN not set!")
+
